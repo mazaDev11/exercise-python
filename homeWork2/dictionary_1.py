7aa6caafe00aa7e08a3667b022e1b7772d959646
baldorj <baldorjc@gmail.com>
2025-02-19 16:17:03 +0000

    Add dictionaries and pandas DataFrame examples

---
+++ homeWork2/dictionary_1.py
@@ -0,0 +1,1 @@
+dict = {'firstName': 'Bat', 'gender': 'M', 'salary': 1.5, 'dateOfBirth': '2000-01-15'}
---
+++ homeWork2/dictionary_2.py
@@ -0,0 +1,7 @@
+dict = [
+    {'firstName': 'Bat', 'gender': 'M', 'salary': 1.5, 'dateOfBirth': '2000-01-15'},
+    {'firstName': 'Saraa', 'gender': 'F', 'salary': 2.0, 'dateOfBirth': '1998-06-21'},
+    {'firstName': 'Bold', 'gender': 'M', 'salary': 3.5, 'dateOfBirth': '1990-08-05'},
+    {'firstName': 'Anu', 'gender': 'F', 'salary': 2.4, 'dateOfBirth': '2001-10-30'},
+    {'firstName': 'Ganaa', 'gender': 'M', 'salary': 3.0, 'dateOfBirth': '1996-04-16'}
+]
